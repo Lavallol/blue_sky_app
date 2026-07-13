cb55764c26391257ad5d1d5397c9399d8d5d7467
Lavallol <lavallol@example.com>
2026-07-13 08:16:49 +0000

    Agregar import MovimientoStock en vista de conteo

--- inventario_app/views_conteo_nuevo.py
+++ inventario_app/views_conteo_nuevo.py
@@ -10,6 +10,8 @@ from django.contrib.auth.decorators import login_required
 
 from inventario_app.models import Producto
 from inventario_app.models import ConteoSesion, ConteoLinea
+from inventario_app.models import MovimientoStock 
+
 from inventario_app.services.actualizar_stock import ServicioActualizarStock
 
 # ============================================================

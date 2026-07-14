42d05464d8698a0e32da2c5803b8c8ed96c5e4fd
Lavallol <lavallol@example.com>
2026-07-14 09:26:46 +0000

    Usar referencia lazy para ConteoSesion en MovimientoStock

--- inventario_app/models.py
+++ inventario_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.utils import timezone
-from inventario_app.models import ConteoSesion
 
 # =========================================================
 # FUNCIÓN PARA GENERAR CÓDIGO INTERNO AUTOMÁTICO
@@ -199,7 +198,7 @@ class MovimientoStock(models.Model):
 
     # ⭐ CAMPO QUE FALTABA
     sesion = models.ForeignKey(
-        ConteoSesion,
+        'inventario_app.ConteoSesion',
         on_delete=models.SET_NULL,
         null=True,
         blank=True,

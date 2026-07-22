868449f071a3adda4146c496f4d43ba68239e38b
Lavallol <lavallol@example.com>
2026-07-22 08:59:36 +0000

    Mover lista actions fuera del fieldset y activar eliminar_albaranes

--- appcompras/admin.py
+++ appcompras/admin.py
@@ -294,7 +294,7 @@ class AlbaranCompraAdmin(admin.ModelAdmin):
             "fields": ("proveedor", "fecha_recepcion", "estado", "total")
         }),
         ("Mercancía recibida desde Pedido", {
-        actions = ["reabrir_albaranes", "accion_copiar_lineas_desde_pedido", "cancelar_albaranes"]            "fields": ("pedido",),
+            "fields": ("pedido",),
             "description": "Seleccione un Pedido pendiente para copiar sus líneas al Albarán."
         }),
     )

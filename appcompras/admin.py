762a8adffa47f1610dfa7e6ea72cdc03f9474a4e
Lavallol <lavallol@example.com>
2026-08-04 11:59:01 +0000

    autocomplete_fields = ['producto'] en PedidoCompraLineaInline

--- appcompras/admin.py
+++ appcompras/admin.py
@@ -109,6 +109,8 @@ class PedidoCompraLineaInline(LineaAutocompletableMixin, admin.TabularInline):
     model = PedidoCompraLinea
     form = PedidoCompraLineaForm
 
+    autocomplete_fields = ['producto']   # ← ESTA ES LA LÍNEA NUEVA
+
     extra = 0
     min_num = 1
     can_delete = True

f4eb0bad1587cda7bb328594235807c2277bd85a
Lavallol <lavallol@example.com>
2026-08-05 08:28:16 +0000

    Fix pedido_inline_fix.js: DOMContentLoaded + django.jQuery

--- static/js/pedido_inline_fix.js
+++ static/js/pedido_inline_fix.js
@@ -1,19 +1,23 @@
-console.log("pedido_inline_fix.js cargado correctamente");
+document.addEventListener("DOMContentLoaded", function() {
 
-django.jQuery(function($) {
+    console.log("pedido_inline_fix.js cargado correctamente");
 
-    function initSelect2(row) {
-        row.find('select.admin-autocomplete').each(function() {
-            $(this).djangoAdminSelect2();
+    (function($) {
+
+        function initSelect2(row) {
+            row.find('select.admin-autocomplete').each(function() {
+                $(this).djangoAdminSelect2();
+            });
+        }
+
+        // Inicializar las filas existentes
+        initSelect2($('.inline-group .form-row'));
+
+        // Inicializar las filas nuevas cuando se agregan
+        $(document).on('formset:added', function(event, row) {
+            initSelect2(row);
         });
-    }
 
-    // Inicializar las filas existentes
-    initSelect2($('.inline-group .form-row'));
-
-    // Inicializar las filas nuevas cuando se agregan
-    django.jQuery(document).on('formset:added', function(event, row) {
-        initSelect2(row);
-    });
+    })(django.jQuery);
 
 });

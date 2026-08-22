08964af434e01f7acb67db46ceb611a57380f263
Lavallol <lavallol@example.com>
2026-08-22 11:23:49 +0000

    MEFIE: restaurar filtro albaranes__isnull en selector de Pedido para Albarán

--- appcompras/admin.py
+++ appcompras/admin.py
@@ -558,7 +558,7 @@ class AlbaranCompraAdmin(admin.ModelAdmin):
                 kwargs["queryset"] = PedidoCompra.objects.filter(
                     proveedor=albaran.proveedor,
                     estado__in=[PedidoCompra.BORRADOR, PedidoCompra.CONFIRMADO]
-                ).exclude(albarancompra__isnull=False)
+                ).exclude(albaranes__isnull=False)
             else:
                 # Caso 2: Creando un albarán nuevo
                 proveedor_id = None
@@ -577,7 +577,7 @@ class AlbaranCompraAdmin(admin.ModelAdmin):
                     kwargs["queryset"] = PedidoCompra.objects.filter(
                         proveedor_id=proveedor_id,
                         estado__in=[PedidoCompra.BORRADOR, PedidoCompra.CONFIRMADO]
-                    ).exclude(albarancompra__isnull=False)
+                    ).exclude(albaranes__isnull=False)
                 else:
                     kwargs["queryset"] = PedidoCompra.objects.none()
 

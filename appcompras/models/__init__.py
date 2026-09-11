fc0451249a1ee3714825973aeace8f263ef9d65d
Lavallol <lavallol@example.com>
2026-09-11 07:25:19 +0000

    MEFIE: Registrar modelo intermedio real en __init__.py

--- appcompras/models/__init__.py
+++ appcompras/models/__init__.py
@@ -2,3 +2,4 @@ from .pedido import PedidoCompra, PedidoCompraLinea
 from .albaran import AlbaranCompra, AlbaranCompraLinea
 from .factura import FacturaCompra, FacturaCompraLinea
 from .condicion_pago import CondicionPago
+from .factura_albaran_linea import FacturaCompraAlbaranLinea

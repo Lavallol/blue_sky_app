ec98af0c5bc20a9b8e400156567c8c8af74cfa7d
Lavallol <lavallol@example.com>
2026-09-12 08:30:35 +0000

    Registrar FacturaCompraAlbaran en paquete models

--- appcompras/models/__init__.py
+++ appcompras/models/__init__.py
@@ -3,4 +3,4 @@ from .albaran import AlbaranCompra, AlbaranCompraLinea
 from .factura import FacturaCompra, FacturaCompraLinea
 from .condicion_pago import CondicionPago
 from .factura_albaran_linea import FacturaCompraAlbaranLinea
-# from ..models import FacturaCompraAlbaran
+from .factura_compra_albaran import FacturaCompraAlbaran
